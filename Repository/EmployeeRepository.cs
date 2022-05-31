d18c665fcd905386c05a23bf6067087421d5c0ec
james chalmers <james.chalmers184@gmail.com>
2022-05-31 07:53:24 +0100

    Updated as far as Chapter 19 Sorting

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -19,6 +19,7 @@ namespace Repository
            trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
             .Search(employeeParameters.SearchTerm)
+            .Sort(employeeParameters.OrderBy)
             .OrderBy(e => e.Name)
             .ToListAsync();
             return PagedList<Employee>
--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -23,5 +23,7 @@ namespace Shared.RequestFeatures
             }
         }
 
+        public string? OrderBy { get; set; }
+
     }
 }
