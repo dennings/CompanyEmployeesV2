bf02d544eae201141c66e10c13dcadf96170108e
james chalmers <james.chalmers184@gmail.com>
2022-05-31 07:19:43 +0100

    Updated to chapter 16 Pagination

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -10,13 +10,14 @@ namespace CompanyEmployees.Extensions
     public static class ServiceExtensions
     {
         public static void ConfigureCors(this IServiceCollection services) =>
-                 services.AddCors(options =>
-                 {
-                     options.AddPolicy("CorsPolicy", builder =>
-                     builder.AllowAnyOrigin()
-                     .AllowAnyMethod()
-                     .AllowAnyHeader());
-                 });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("X-Pagination"));
+            });
 
 
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
