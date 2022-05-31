f61afa8ea59b1e16c8c8fb72bc3f21ad3e4bce13
james chalmers <james.chalmers184@gmail.com>
2022-05-31 07:35:28 +0100

    Updated as far as Chapter 18 Searching

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -2,6 +2,8 @@
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Shared.RequestFeatures;
+using Repository.Extensions;
+
 
 namespace Repository
 {
@@ -13,14 +15,17 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age
-           >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
+           trackChanges)
+            .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+            .Search(employeeParameters.SearchTerm)
             .OrderBy(e => e.Name)
             .ToListAsync();
             return PagedList<Employee>
             .ToPagedList(employees, employeeParameters.PageNumber,
            employeeParameters.PageSize);
         }
+
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
             await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
